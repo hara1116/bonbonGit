94c32b527fccbf125e8b422c23b38f24dbf150e5
哲平 <@Lenovo-PC>
2017-10-02 15:53:21 +0900

    aaaa

--- src/main/java/sa/bonbon/action/IndexAction.java
+++ src/main/java/sa/bonbon/action/IndexAction.java
@@ -18,7 +18,7 @@ package sa.bonbon.action;
 import org.seasar.struts.annotation.Execute;
 
 public class IndexAction {
-	
+	//
     @Execute(validator = false)
 	public String index() {
         return "index.jsp";
