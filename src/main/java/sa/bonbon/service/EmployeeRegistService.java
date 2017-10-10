a9e0cd0373ca8a8ab5e028318b848acb179766c0
哲平 <@Lenovo-PC>
2017-10-10 15:47:07 +0900

    最新マージ

--- src/main/java/sa/bonbon/service/EmployeeRegistService.java
+++ src/main/java/sa/bonbon/service/EmployeeRegistService.java
@@ -10,7 +10,9 @@ import javax.annotation.Resource;
 import org.seasar.extension.jdbc.JdbcManager;
 import org.seasar.framework.container.SingletonS2Container;
 
+import sa.bonbon.entity.MEmployee;
 import sa.bonbon.entity.MAdmin;
+import sa.bonbon.form.EmployeeRegistForm;
 import sa.bonbon.form.UserregistForm;
 
 public class EmployeeRegistService {
@@ -26,23 +28,27 @@ public class EmployeeRegistService {
 	 * @param password    電話番号
 	 * @return 登録数
 	 */
-	public int userRegistration(UserregistForm userregistForm) {
+	public int employeeRegist(EmployeeRegistForm employeeRegistForm) {
 
 		DateFormat df1 = new SimpleDateFormat("yyyyMMdd");
         Date date1 = new Date();
         String today = df1.format(date1);
 
-        MAdmin entity = new MAdmin();
-        entity.adminCd = userregistForm.adminCd;                        //従業員CD
-        entity.password = userregistForm.password;                      //パスワード
-        entity.adminName = userregistForm.adminName;                    //名前
-        entity.telNo = userregistForm.telNo;                            //電話番号
+        MEmployee entity = new MEmployee();
+        entity.employeeCd = employeeRegistForm.employeeCd;              //社員CD
+        entity.password = employeeRegistForm.password;                  //パスワード
+        entity.employeeName = employeeRegistForm.employeeName;          //名前
+        entity.birthday = employeeRegistForm.birthDay;                  //生年月日
+        entity.telNo = employeeRegistForm.telNo;                        //電話番号
+        entity.sex = employeeRegistForm.sex;                            //性別
+        entity.syozokuShop = employeeRegistForm.shozokuShop;            //所属店舗
+        entity.status = employeeRegistForm.status;                      //ステータス
 
         entity.startYmd = Integer.parseInt(today);                      //開始日
         entity.endYmd = 99991231;                                       //終了日
 
-        entity.createUser = userregistForm.adminCd;                     //登録者
-        entity.lastupdateUser = userregistForm.adminCd;                 //最終更新者
+        entity.createUser = employeeRegistForm.adminCd;                 //登録者
+        entity.lastupdateUser = employeeRegistForm.adminCd;             //最終更新者
         entity.createDate = new Timestamp(System.currentTimeMillis());  //更新日
         entity.lastupdate = new Timestamp(System.currentTimeMillis());  //最終更新日
 
